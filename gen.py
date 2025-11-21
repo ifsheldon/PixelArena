a3d357c8302ecc421c10612746898b556a3e7470
ifsheldon <feng.liang@kaust.edu.sa>
2025-11-21 19:45:30 +0800

    add gemini 3 image zero shot

---
+++ gen.py
@@ -0,0 +1,131 @@
+from google import genai
+from google.genai import types
+from pathlib import Path
+from PIL import Image
+import asyncio
+import tqdm
+
+client = genai.Client(
+    api_key="AIzaSyC54rwWvN69P0scm1vEd4YRzk7smrPWzJs",
+).aio
+
+
+def save_binary_file(file_name, data):
+    f = open(file_name, "wb")
+    f.write(data)
+    f.close()
+    print(f"File saved to to: {file_name}")
+
+
+async def gen_mask(
+    original_image_path: Path,
+    color_palette_path: Path,
+    output_dir: Path,
+    attempts: int,
+):
+    prompt = """I want you to do semantic segmentation based on facial features. 
+
+The label encodings are
+
+```
+[
+    [0, 0, 0],  # 0 background
+    [204, 0, 0],  # 1 skin
+    [76, 153, 0],  # 2 nose
+    [204, 204, 0],  # 3 eye_glass
+    [51, 51, 255],  # 4 left_eye
+    [204, 0, 204],  # 5 right_eye
+    [0, 255, 255],  # 6 left_eyebrow
+    [255, 204, 204],  # 7 right_eyebrow
+    [102, 51, 0],  # 8 left_ear
+    [255, 0, 0],  # 9 right_ear
+    [102, 204, 0],  # 10 mouth
+    [255, 255, 0],  # 11 upper_lip
+    [0, 0, 153],  # 12 lower_lip
+    [0, 0, 204],  # 13 hair
+    [255, 51, 153],  # 14 hat
+    [0, 204, 204],  # 15 ear_ring
+    [0, 51, 0],  # 16 neck_lace
+    [255, 153, 51],  # 17 neck
+    [0, 204, 0],  # 18 cloth
+]
+```
+
+For you convenience, I've also give you a color palette (the second image) for the label encodings.
+
+Please draw a colorful mask, given the photo (the first image), the color palette and the label encodings. 
+
+Note that for the left and right used by the labels, these are with respect to the person in the image, NOT the image itself, so the left facial features of the person are on the right of the image. 
+Check if you have labeled the features on the left of the image to be the right feature labels.
+"""
+    contents = [
+        Image.open(original_image_path).convert("RGB"),
+        Image.open(color_palette_path).convert("RGB"),
+        prompt,
+    ]
+
+    original_file_name = original_image_path.stem
+
+    for attempt_idx in range(attempts):
+        response = await client.models.generate_content(
+            model="gemini-3-pro-image-preview",
+            contents=contents,
+            config=types.GenerateContentConfig(
+                temperature=1.0,
+                response_modalities=[
+                    "IMAGE",
+                    "TEXT",
+                ],
+                image_config=types.ImageConfig(
+                    aspect_ratio="1:1",
+                    image_size="1K",
+                ),
+                top_p=0.95,
+                thinking_config=types.ThinkingConfig(include_thoughts=True),
+            ),
+        )
+
+        for part in response.parts:
+            # if part.thought:
+            #     if part.text:
+            #         print(part.text)
+            #     elif image := part.as_image():
+            #         image.show()
+            if part.text is not None:
+                # print(part.text)
+                pass
+            elif image := part.as_image():
+                image.save(
+                    f"{output_dir}/{original_file_name}.mask.{attempt_idx}.raw.jpg"
+                )
+
+        await asyncio.sleep(2)
+
+
+async def batch_processing():
+    image_dir = Path("./train/images")
+    all_images = list(image_dir.glob("*.jpg"))
+    batch_size = 5
+    color_palette_path = Path("seg-labels.png")
+    output_dir = Path("test")
+    attempts = 3
+    processed_images = list(output_dir.glob(f"*.mask.{attempts - 1}.raw.jpg"))
+    processed_image_names = {image.stem.split(".")[0] for image in processed_images}
+    print(f"Processed {len(processed_image_names)} images before")
+    all_images_todo = [
+        image for image in all_images if image.stem.split(".")[0] not in processed_image_names
+    ]
+    print(f"Processing {len(all_images_todo)} images")
+    for i in tqdm.tqdm(range(0, len(all_images_todo), batch_size)):
+        batch_images = all_images_todo[i : i + batch_size]
+        await asyncio.gather(
+            *[
+                gen_mask(image, color_palette_path, output_dir, attempts)
+                for image in batch_images
+            ]
+        )
+        await asyncio.sleep(1)
+
+
+if __name__ == "__main__":
+    asyncio.run(batch_processing())
