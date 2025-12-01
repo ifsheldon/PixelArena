bd194528e708922eaaf50f32fe711cae814bf5d7
ifsheldon <feng.liang@kaust.edu.sa>
2025-12-01 20:12:03 +0800

    improved script

--- generate_mask_gemini_pro.py
+++ generate_mask_gemini_pro.py
@@ -11,10 +11,12 @@ import asyncio
 from tqdm.asyncio import tqdm_asyncio
 import random
 import logging
-from pixel_arena.dataset_utils.celeb_a_mask_hq import get_prompt
+from pixel_arena.dataset_utils.celeb_a_mask_hq import get_prompt as get_prompt_celeb
+from pixel_arena.dataset_utils.coco import get_prompt as get_prompt_coco
 from asynciolimiter import Limiter
-from typing import List
+from typing import List, Literal
 import os
+import pickle
 
 # parameters when it's run in CLI
 MODEL = "gemini-3-pro-image-preview"
@@ -61,33 +63,35 @@ CLIENTS = [
 ]
 
 RPM = 12  # tier 1 -> RPM = 20 with a bit buffer
-LIMITERS = [Limiter(RPM / 60) for _ in range(len(CLIENTS))]
 
 logger = logging.getLogger("generate_mask_gemini")
 logging.basicConfig(level=logging.WARNING)
 
 
-async def get_client(idx=None) -> AsyncClient:
-    if idx is None:
-        idx = random.randint(0, len(CLIENTS) - 1)
-    else:
-        assert 0 <= idx < len(CLIENTS)
-
-    limiter = LIMITERS[idx]
-    await limiter.wait()
-    return CLIENTS[idx]
-
-
 async def gen_mask(
+    *,
+    client: AsyncClient,
+    limiter: Limiter,
+    model: Literal["gemini-3-pro-image-preview", "gemini-2.5-flash-image"],
+    dataset: Literal["celeb", "coco"],
     original_image_path: Path,
     color_palette_path: Path,
     output_dir: Path,
     attempt_idx: int,
     label_colors: List[List[int]] | None,
-    client_idx: int | None,
+    save_response: bool,
 ):
+    assert model in ["gemini-3-pro-image-preview", "gemini-2.5-flash-image"]
+    assert dataset in ["celeb", "coco"]
+    if dataset == "celeb":
+        get_prompt = get_prompt_celeb
+    else:
+        get_prompt = get_prompt_coco
     original_file_name = original_image_path.stem
     save_file_path = f"{output_dir}/{original_file_name}.mask.{attempt_idx}.raw.jpg"
+    save_response_path = (
+        f"{output_dir}/{original_file_name}.mask.{attempt_idx}.response.pkl"
+    )
 
     if os.path.exists(save_file_path):
         logger.info(f"Mask for {original_file_name} already exists at {save_file_path}")
@@ -103,15 +107,15 @@ async def gen_mask(
 
     thinking_config = (
         types.ThinkingConfig(include_thoughts=True)
-        if MODEL == "gemini-3-pro-image-preview"
+        if model == "gemini-3-pro-image-preview"
         else None
     )
-    image_size = "1K" if MODEL == "gemini-3-pro-image-preview" else None
+    image_size = "1K" if model == "gemini-3-pro-image-preview" else None
 
     try:
-        client = await get_client(client_idx)
+        await limiter.wait()
         response = await client.models.generate_content(
-            model=MODEL,
+            model=model,
             contents=contents,
             config=types.GenerateContentConfig(
                 temperature=1.0,
@@ -133,18 +137,14 @@ async def gen_mask(
             return
 
         for part in response.parts:
-            # if part.thought:
-            #     if part.text:
-            #         print(part.text)
-            #     elif image := part.as_image():
-            #         image.show()
-            if part.text is not None:
-                # print(part.text)
-                pass
-            elif image := part.as_image():
+            if image := part.as_image():
                 image.save(save_file_path)
             else:
                 pass
+
+        if save_response:
+            with open(save_response_path, "wb") as f:
+                pickle.dump(response, f)
 
     except Exception as e:
         logger.warning(
@@ -152,31 +152,42 @@ async def gen_mask(
         )
 
 
-def get_img_id(image_path: Path) -> str:
-    return image_path.stem.split(".")[0]
-
-
 async def batch_processing(
+    *,
+    clients: List[AsyncClient] | AsyncClient,
+    rpm: int,
+    model: Literal["gemini-3-pro-image-preview", "gemini-2.5-flash-image"],
+    dataset: Literal["celeb", "coco"],
     image_dir: Path,
     output_dir: Path,
     color_palette_path: Path,
     attempts: int,
     label_colors: List[List[int]] | None,
-    client_idx: int | None,
+    save_response: bool,
 ):
     all_images = list(image_dir.glob("*.jpg"))
+    if isinstance(clients, AsyncClient):
+        clients = [clients]
+    limiters = [Limiter(rpm / 60) for _ in range(len(clients))]
     tasks = []
 
     for image in all_images:
         for attempt_idx in range(attempts):
+            idx = random.randint(0, len(clients) - 1)
+            client = clients[idx]
+            limiter = limiters[idx]
             tasks.append(
                 gen_mask(
+                    client=client,
+                    limiter=limiter,
+                    model=model,
+                    dataset=dataset,
                     original_image_path=image,
                     color_palette_path=color_palette_path,
                     output_dir=output_dir,
                     label_colors=label_colors,
-                    client_idx=client_idx,
                     attempt_idx=attempt_idx,
+                    save_response=save_response,
                 )
             )
 
@@ -188,11 +199,15 @@ async def batch_processing(
 if __name__ == "__main__":
     asyncio.run(
         batch_processing(
+            clients=CLIENTS,
+            rpm=RPM,
+            model=MODEL,
             image_dir=IMAGE_DIR,
             output_dir=OUTPUT_DIR,
             color_palette_path=COLOR_PALETTE_PATH,
             attempts=ATTEMPTS,
             label_colors=None,
-            client_idx=CLIENT_IDX,
+            save_response=False,
+            dataset="celeb",
         )
     )
