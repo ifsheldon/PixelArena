f886fb3fcfe199cc539ff6f5727eedb4d8afd24d
ifsheldon <feng.liang@kaust.edu.sa>
2025-12-02 22:35:22 +0800

    add metrics calculation

---
+++ calculate_metrics.py
@@ -0,0 +1,109 @@
+from pathlib import Path
+from multiprocessing import Pool
+from pixel_arena.metrics import RunInfo
+
+
+gemini_pro_run_celeb = RunInfo(
+    model_name="gemini-pro",
+    small_mask=False,
+    pred_mask_path=Path("./results/celeb/gemini-pro-150"),
+    attempts=5,
+    dataset="celeb",
+)
+gemini_pro_shuffled_run_celeb = RunInfo(
+    model_name="gemini-pro-shuffled",
+    small_mask=False,
+    pred_mask_path=Path("./results/celeb/gemini-pro-150-shuffled-label-colors"),
+    attempts=5,
+    dataset="celeb",
+)
+gemini_run_celeb = RunInfo(
+    model_name="gemini",
+    small_mask=False,
+    pred_mask_path=Path("./results/celeb/gemini-150"),
+    attempts=5,
+    dataset="celeb",
+)
+gpt_run_celeb = RunInfo(
+    model_name="gpt-image",
+    small_mask=False,
+    pred_mask_path=Path("./results/celeb/gpt-image-150"),
+    attempts=5,
+    dataset="celeb",
+)
+sam3_run_celeb = RunInfo(
+    model_name="sam3",
+    small_mask=True,
+    pred_mask_path=Path("./results/celeb/sam3-150"),
+    attempts=1,
+    dataset="celeb",
+)
+segface_run_celeb = RunInfo(
+    model_name="segface",
+    small_mask=True,
+    pred_mask_path=Path("./results/celeb/segface-150"),
+    attempts=1,
+    dataset="celeb",
+)
+uni_moe_2_image_run_celeb = RunInfo(
+    model_name="uni-moe-2-image",
+    small_mask=False,
+    pred_mask_path=Path("./results/celeb/uni-moe-2-image-150"),
+    attempts=1,
+    dataset="celeb",
+)
+uni_moe_2_omni_run_celeb = RunInfo(
+    model_name="uni-moe-2-omni",
+    small_mask=False,
+    pred_mask_path=Path("./results/celeb/uni-moe-2-omni-150"),
+    attempts=1,
+    dataset="celeb",
+)
+# emu35_run_celeb = RunInfo(
+#     model_name="emu35",
+#     small_mask=True,
+#     pred_mask_path=Path("./results/celeb/emu35-150"),
+#     attempts=1,
+#     dataset="celeb",
+# )
+
+celeb_runs = [
+    gemini_pro_run_celeb,
+    gemini_pro_shuffled_run_celeb,
+    gemini_run_celeb,
+    gpt_run_celeb,
+    sam3_run_celeb,
+    segface_run_celeb,
+    uni_moe_2_image_run_celeb,
+    uni_moe_2_omni_run_celeb,
+]
+
+gemini_pro_run_coco = RunInfo(
+    model_name="gemini-pro",
+    small_mask=False,
+    pred_mask_path=Path("./results/coco/gemini-pro-150"),
+    attempts=5,
+    dataset="coco",
+)
+gemini_run_coco = RunInfo(
+    model_name="gemini",
+    small_mask=False,
+    pred_mask_path=Path("./results/coco/gemini-150"),
+    attempts=5,
+    dataset="coco",
+)
+
+coco_runs = [
+    gemini_pro_run_coco,
+    gemini_run_coco,
+]
+
+
+if __name__ == "__main__":
+    runs = coco_runs
+
+    def calc(run: RunInfo):
+        run.calculate_and_save_metrics()
+
+    with Pool(len(runs)) as pool:
+        pool.map(calc, runs)
---
+++ pixel_arena/metrics.py
@@ -0,0 +1,162 @@
+import torch
+from PIL import Image
+from torchvision import transforms
+from pathlib import Path
+from functools import cache
+from torchmetrics.functional.classification import multiclass_f1_score
+from torchmetrics.functional.segmentation import mean_iou, dice_score
+from typing import List, Literal
+import pickle
+from tqdm.auto import tqdm
+
+to_tensor = transforms.PILToTensor()
+
+
+@cache
+def get_image(path: Path, return_image: bool) -> Image.Image:
+    mask = Image.open(path)
+    if return_image:
+        return mask
+    mask = to_tensor(mask)
+    return mask
+
+
+class RunInfo:
+    def __init__(
+        self,
+        *,
+        model_name: str,
+        small_mask: bool,
+        pred_mask_path: Path,
+        attempts: int,
+        dataset: Literal["celeb", "coco"],
+    ):
+        self.model_name = model_name
+        self.small_mask = small_mask
+        self.pred_mask_path = pred_mask_path
+        assert pred_mask_path.exists()
+        self.attempts = attempts
+        self.dataset = dataset
+        self.mask_transform = transforms.PILToTensor()
+        self.save_metric_path = Path(
+            f"saved_result_binary/metrics/{dataset}/{model_name}.metrics.pkl"
+        )
+
+        if dataset == "celeb":
+            image_path = Path("./eval-set/celeb/images-150/")
+            assert image_path.exists()
+        else:
+            image_path = Path("./eval-set/coco/images-150/")
+            assert image_path.exists()
+
+        image_ids = {image.stem for image in image_path.glob("*.jpg")}
+        self.image_ids = sorted(list(image_ids))
+
+        if dataset == "celeb":
+            self.ref_masks_path = (
+                Path("./eval-set/celeb/masks-512/")
+                if small_mask
+                else Path("./eval-set/celeb/masks-1024/")
+            )
+            self.class_max = 18
+            self.label_num = 19
+        else:
+            assert not small_mask
+            self.ref_masks_path = Path("./eval-set/coco/masks-1024/")
+            self.class_max = 200
+            self.label_num = 201
+        assert self.ref_masks_path.exists()
+
+    def get_mask_ref(
+        self, *, image_id: str, return_image: bool
+    ) -> torch.Tensor | Image.Image:
+        ref_mask_path = self.ref_masks_path / f"{image_id}.png"
+        mask = get_image(ref_mask_path, return_image)
+        if return_image:
+            return mask
+        assert mask.dtype == torch.uint8
+        assert mask.max() <= self.class_max
+        return mask
+
+    def get_mask_preds(
+        self, *, image_id: str, return_image: bool
+    ) -> List[torch.Tensor] | List[Image.Image]:
+        result_path = self.pred_mask_path
+        masks = []
+        for i in range(self.attempts):
+            mask_path = result_path / f"{image_id}.mask.{i}.pred.png"
+            mask = Image.open(mask_path)
+            if return_image:
+                masks.append(mask)
+                continue
+            mask = self.mask_transform(mask)
+            assert mask.dtype == torch.uint8
+            assert mask.max() <= self.class_max
+            masks.append(mask)
+        return masks
+
+    def calculate_and_save_metrics(self):
+        f1_scores = []
+        iou_scores = []
+        dice_scores = []
+        for image_id in tqdm(self.image_ids):
+            mask_ref = self.get_mask_ref(image_id=image_id, return_image=False)
+            masks_pred = self.get_mask_preds(image_id=image_id, return_image=False)
+            assert mask_ref.shape == masks_pred[0].shape
+
+            f1_score = torch.empty(self.attempts)
+            for i, mask_pred in enumerate(masks_pred):
+                f1 = multiclass_f1_score(
+                    mask_pred, mask_ref, num_classes=self.label_num, average="macro"
+                )
+                f1_score[i] = f1
+            f1_scores.append(f1_score)
+
+            mask_ref = mask_ref.long()
+            for i in range(self.attempts):
+                masks_pred[i] = masks_pred[i].long()
+
+            iou_score = torch.empty(self.attempts)
+            for i, mask_pred in enumerate(masks_pred):
+                iou = mean_iou(
+                    mask_pred,
+                    mask_ref,
+                    num_classes=self.label_num,
+                    input_format="index",
+                )
+                iou_score[i] = iou
+            iou_scores.append(iou_score)
+
+            dice = torch.empty(self.attempts)
+            for i, mask_pred in enumerate(masks_pred):
+                d = dice_score(
+                    mask_pred,
+                    mask_ref,
+                    num_classes=self.label_num,
+                    average="macro",
+                    input_format="index",
+                )
+                dice[i] = d
+            dice_scores.append(dice)
+
+        f1_scores = torch.stack(f1_scores)
+        iou_scores = torch.stack(iou_scores)
+        dice_scores = torch.stack(dice_scores)
+        self.f1_scores = f1_scores
+        self.iou_scores = iou_scores
+        self.dice_scores = dice_scores
+        with open(self.save_metric_path, "wb") as f:
+            metrics = {
+                "f1": f1_scores,
+                "iou": iou_scores,
+                "dice": dice_scores,
+            }
+            pickle.dump(metrics, f)
+
+    def load_metrics(self):
+        with open(self.save_metric_path, "rb") as f:
+            metrics = pickle.load(f)
+        self.f1_scores = metrics["f1"]
+        self.iou_scores = metrics["iou"]
+        self.dice_scores = metrics["dice"]
+        return self.f1_scores, self.iou_scores, self.dice_scores
